9b5b9765c099aab52d2b0e96e621d7ccb8db7e32
daniel-farkas <daniel.farkas@codecentric.de>
2014-10-15 23:38:49 +0200

    simplify security filter to avoid overhead and improve performance

--- src/main/java/org/bundolo/security/RestAuthenticationProvider.java
+++ src/main/java/org/bundolo/security/RestAuthenticationProvider.java
@@ -6,7 +6,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang3.StringUtils;
-import org.bundolo.Constants;
 import org.bundolo.SecurityUtils;
 import org.bundolo.dao.UserProfileDAO;
 import org.bundolo.model.UserProfile;
@@ -27,56 +26,84 @@ public class RestAuthenticationProvider implements AuthenticationProvider {
     private UserProfileDAO userProfileDAO;
 
     @Override
-    // @Transactional(propagation = Propagation.NEVER, rollbackFor = Exception.class)
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 	// logger.log(Level.WARNING, "authenticate authentication: " + authentication);
 	UsernamePasswordAuthenticationToken restToken = (UsernamePasswordAuthenticationToken) authentication;
 
 	String key = (String) restToken.getPrincipal();
 	String credentials = (String) restToken.getCredentials();
-	// TODO consider performance here, we do db query even if user did not provide username and password
+	// this is the simplest version, not processing anonymous requests
 	List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-	String proposedUsername;
-	String proposedPassword;
-	if (StringUtils.isBlank(key)) {
-	    proposedUsername = Constants.DEFAULT_GUEST_USERNAME;
-	} else {
-	    proposedUsername = key;
-	}
-	if (StringUtils.isBlank(credentials)) {
-	    proposedPassword = " ";
-	} else {
-	    proposedPassword = credentials;
-	}
-	UserProfile userProfile = userProfileDAO.findByField("username", proposedUsername);
-	String dbSalt;
-	String dbPassword;
-	UserProfileStatusType dbStatus;
-	if (userProfile != null) {
-	    dbSalt = userProfile.getSalt();
-	    dbPassword = userProfile.getPassword();
-	    dbStatus = userProfile.getUserProfileStatus();
-	} else {
-	    dbSalt = "";
-	    dbPassword = "";
-	    dbStatus = UserProfileStatusType.disabled;
-	}
-	boolean credentialsValid;
-	try {
-	    credentialsValid = SecurityUtils.getHashWithPredefinedSalt(proposedPassword, dbSalt).equals(dbPassword)
-		    && (UserProfileStatusType.active.equals(dbStatus));
-	} catch (Exception ex) {
-	    logger.log(Level.SEVERE, "authenticateUser exception: " + ex);
-	    credentialsValid = false;
-	}
-	if (credentialsValid) {
-	    authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-	} else {
-	    authorities.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+
+	if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(credentials)) {
+	    UserProfile userProfile = userProfileDAO.findByField("username", key);
+	    if (userProfile != null) {
+		try {
+		    if (SecurityUtils.getHashWithPredefinedSalt(credentials, userProfile.getSalt()).equals(
+			    userProfile.getPassword())
+			    && (UserProfileStatusType.active.equals(userProfile.getUserProfileStatus()))) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		    }
+		} catch (Exception ex) {
+		    logger.log(Level.SEVERE, "authenticateUser exception: " + ex);
+		}
+	    }
 	}
 	return new UsernamePasswordAuthenticationToken(key, credentials, authorities);
     }
 
+    // this version is safer because each request is checked, but there is extra query in case of anonymous access
+    /*    
+        @Override
+        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    	// logger.log(Level.WARNING, "authenticate authentication: " + authentication);
+    	UsernamePasswordAuthenticationToken restToken = (UsernamePasswordAuthenticationToken) authentication;
+
+    	String key = (String) restToken.getPrincipal();
+    	String credentials = (String) restToken.getCredentials();
+    	// TODO consider performance here, we do db query even if user did not provide username and password
+    	List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+    	String proposedUsername;
+    	String proposedPassword;
+    	if (StringUtils.isBlank(key)) {
+    	    proposedUsername = Constants.DEFAULT_GUEST_USERNAME;
+    	} else {
+    	    proposedUsername = key;
+    	}
+    	if (StringUtils.isBlank(credentials)) {
+    	    proposedPassword = " ";
+    	} else {
+    	    proposedPassword = credentials;
+    	}
+    	UserProfile userProfile = userProfileDAO.findByField("username", proposedUsername);
+    	String dbSalt;
+    	String dbPassword;
+    	UserProfileStatusType dbStatus;
+    	if (userProfile != null) {
+    	    dbSalt = userProfile.getSalt();
+    	    dbPassword = userProfile.getPassword();
+    	    dbStatus = userProfile.getUserProfileStatus();
+    	} else {
+    	    dbSalt = "";
+    	    dbPassword = "";
+    	    dbStatus = UserProfileStatusType.disabled;
+    	}
+    	boolean credentialsValid;
+    	try {
+    	    credentialsValid = SecurityUtils.getHashWithPredefinedSalt(proposedPassword, dbSalt).equals(dbPassword)
+    		    && (UserProfileStatusType.active.equals(dbStatus));
+    	} catch (Exception ex) {
+    	    logger.log(Level.SEVERE, "authenticateUser exception: " + ex);
+    	    credentialsValid = false;
+    	}
+    	if (credentialsValid) {
+    	    authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+    	} else {
+    	    authorities.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+    	}
+    	return new UsernamePasswordAuthenticationToken(key, credentials, authorities);
+        }
+    */
     @Override
     /*
         Determines if this class can support the token provided by the filter.
